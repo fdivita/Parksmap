6a7a4321a25b087abc395f466966ff5413bd2245
fabio di vita <fdivita@gmail.com>
2018-01-05 18:52:56 +0100

    Updated Settings

---
+++ parksmap-web/src/test/java/com/openshift/evg/roadshow/test/ParksMapTest1.java
@@ -0,0 +1,19 @@
+package com.openshift.evg.roadshow.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParksMapTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
